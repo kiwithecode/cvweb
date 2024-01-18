e2f3a5e656d5b629fd9d9a19af883b2d3eb38979
Kevin Armas <karmas@libelulasoft.com>
2024-01-18 17:51:15 -0500

    fix revert

--- src/constants/index.js
+++ src/constants/index.js
@@ -50,7 +50,7 @@ export const navLinks = [
 
 const services = [
   {
-    title: "Front-end Developer",
+    title: "Web Developer",
     icon: web,
   },
   {
@@ -58,7 +58,7 @@ const services = [
     icon: mobile,
   },
   {
-    title: "Back-end Developer",
+    title: "Backend Developer",
     icon: backend,
   },
   {
@@ -172,20 +172,9 @@ const experiences = [
     company_name: "PROWESSEC",
     icon: web,
     iconBg: "#383E56",
-    date: "Septiembre 2021 - Noviembre 2021",
+    date: "Septiembre 2021 - Enero 2022",
     points: [
      "Creaba diseños y prototipos para aplicaciones móviles y web en Figma, y brindaba soporte y mantenimiento para asegurar que estuvieran siempre en óptimas condiciones."
-    ],
-  },
-  {
-    title: "Full stack Developer",
-    company_name: "Digital Tempo",
-    icon: web,
-    iconBg: "#E6DEDD",
-    date: "Enero 2022 - Abril 2022",
-    points: [
-     " Como desarrollador de software, mi especialización se centra en la creación y mantenimiento de aplicaciones web dinámicas y eficientes utilizando Laravel con PHP. Mi experiencia abarca desde el desarrollo backend, aprovechando las robustas capacidades de Laravel, hasta la creación de interfaces de usuario interactivas e intuitivas con tecnologías frontend, incluyendo AJAX. Trabajo de la mano con equipos multidisciplinarios, colaborando estrechamente con diseñadores, gerentes de producto y otros desarrolladores para entregar productos de alta calidad que no solo cumplen sino que superan las expectativas"
-
     ],
   },
   {
@@ -197,39 +186,6 @@ const experiences = [
     points: [
       "Como desarrollador de software, mi experiencia incluye el desarrollo y mantenimiento de aplicaciones web utilizando React.js y otras tecnologías relacionadas. Además, colaboro con equipos multifuncionales, como diseñadores, gerentes de producto y otros desarrolladores para crear productos de alta calidad. También implemento diseños receptivos y garantizo la compatibilidad entre navegadores.",
       " QA automático y testing, para garantizar que las aplicaciones que desarrollamos cumplan con los más altos estándares de calidad. Participaré en revisiones de código y proporcionaré comentarios constructivos a otros desarrolladores, para asegurarnos de que todas las partes interesadas estén satisfechas con el resultado final.",
-    ],
-  },
-  {
-    title: "Full stack Developer",
-    company_name: "Digotec",
-    icon: web,
-    iconBg: "#E6DEDD",
-    date: "Abril 2023 - Julio 2023",
-    points: [
-      "En mi trayectoria como desarrollador de software, he acumulado una amplia experiencia en la creación de soluciones tecnológicas completas y multifacéticas. Me especializo en el desarrollo backend utilizando Python y Flask, una combinación que me permite construir aplicaciones robustas y eficientes. En el frontend, aplico mis habilidades en React para crear interfaces de usuario interactivas y modernas, asegurando una experiencia fluida y atractiva para el usuario.",
-      "Además de mi experiencia en desarrollo web, también he incursionado en el ámbito de las aplicaciones móviles utilizando Flutter. Esta herramienta me ha permitido desarrollar aplicaciones móviles multiplataforma de alto rendimiento y con una excelente interfaz de usuario. Mi enfoque en Flutter abarca desde la conceptualización hasta la implementación final, asegurando que las aplicaciones sean tanto técnicamente sólidas como estéticamente agradables."
-    ],
-  },
-  {
-    title: "QA DE ASEGURAMIENTO DE LA CALIDAD",
-    company_name: "EPNTECH",
-    icon: web,
-    iconBg: "#383E56",
-    date: "Agosto 2023 - Noviembre 2023",
-    points: [ 
-      "En EPNTECH, he desempeñado un papel clave como Desarrollador Móvil, especializándome en React Native. Mis responsabilidades incluyen el desarrollo y mantenimiento de aplicaciones móviles, enfocándome en la optimización de interfaces y rendimiento para diferentes dispositivos. Mi enfoque se ha centrado en la creación de soluciones móviles que no solo cumplen con los requisitos técnicos, sino que también proporcionan una experiencia de usuario fluida y atractiva.",
-      "Además, he asumido un rol crucial en el aseguramiento de la calidad (QA) en EPNTECH. Mi trabajo implica la realización de pruebas manuales y automatizadas, utilizando herramientas como Cypress y JUnit para garantizar la calidad y fiabilidad del software. He sido responsable de implementar pruebas unitarias eficientes, asegurando así la robustez y el rendimiento óptimo del código en nuestros proyectos."
-    ],
-  },
-  {
-    title: "Developer Fornted",
-    company_name: "LibelulaSof",
-    icon: web,
-    iconBg: "#383E56",
-    date: " Noviembre 2023 - Actualmente",
-    points: [ 
-      "Como Desarrollador Frontend especializado en React, tengo una amplia experiencia en la creación de interfaces de usuario modernas y dinámicas. Mi experiencia no se limita solo al desarrollo de interfaces; también abarca la integración y manejo eficiente de middleware, lo cual me permite conectar de manera fluida las interfaces de usuario con la lógica de negocio y las fuentes de datos. Esta habilidad es crucial para desarrollar aplicaciones web interactivas y de alto rendimiento.",
-      "Además, poseo una sólida comprensión y experiencia práctica en el trabajo con bases de datos NoSQL. Esta experiencia me permite gestionar datos no estructurados de manera eficiente, lo cual es esencial para las aplicaciones modernas que manejan grandes volúmenes de datos y requieren escalabilidad y flexibilidad. Mi enfoque se centra en asegurar que la arquitectura de datos sea robusta, eficiente y capaz de adaptarse a las necesidades cambiantes de la aplicación y sus usuarios."
     ],
   },
 ];
